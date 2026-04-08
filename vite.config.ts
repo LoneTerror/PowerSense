b4df21f7c518c7195ea2cafd343db1f995afc52e
LoneTerror <nothingshere21@gmail.com>
2026-04-09 00:05:51 +0530

    Updated Dependencies and allowedHosts URL

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ["powersense.site"],
+    allowedHosts: ["powersense.top"],
   },
   resolve: {
     alias: {
